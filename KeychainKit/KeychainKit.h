9d9ea6d134258caafc6e1f2e96267fa89809a80b
Valentin <valentin.mont@ynov.com>
2022-05-09 08:44:18 +0200

    Initial Commit

---
+++ KeychainKit/KeychainKit.h
@@ -0,0 +1,18 @@
+//
+//  KeychainKit.h
+//  KeychainKit
+//
+//  Created by Valentin Mont School on 09/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KeychainKit.
+FOUNDATION_EXPORT double KeychainKitVersionNumber;
+
+//! Project version string for KeychainKit.
+FOUNDATION_EXPORT const unsigned char KeychainKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeychainKit/PublicHeader.h>
+
+
